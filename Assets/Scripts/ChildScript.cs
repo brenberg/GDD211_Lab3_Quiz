56bf51b3df175028f4e1c12d9eb65289edd59f59
brenberg <75150191+brenberg@users.noreply.github.com>
2021-03-08 16:01:23 -0500

    Initial commit

---
+++ Assets/Scripts/ChildScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildScript : ParentScript
+{
+    public string Name;
+    public int Calories;
+    public static int StaticCalories;
+    public int Karmas;
+    public static int StaticKarmas;
+    public bool Alive;
+    public string Message;
+    public static string StaticMessage;
+
+    private void OnMouseDown()
+    {
+        StaticMessage = Message;
+        StaticCalories = Calories;
+        StaticKarmas = Karmas;
+        UseItem();
+    }
+
+
+    public override void UseItem()
+    {
+        base.UseItem();
+        Debug.Log("Put things here");
+    }
+
+}
